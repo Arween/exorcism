d02148fd916e83c82073f2763fba7b97848a2d64
Arween <semka.alesya@gmail.com>
2017-04-30 20:17:37 +0300

    tasks 1,2

--- hw_strings/strings_exercises.py
+++ hw_strings/strings_exercises.py
@@ -1,47 +1,71 @@
-print("Select difficulty level: easy, middle, hard")
-text = input("Level: ")
+# Bob
+# Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+# Bob answers 'Sure.' if you ask him a question.
+# He answers 'Whoa, chill out!' if you yell at him.
+# He says 'Fine. Be that way!' if you address him without actually saying anything.
+# He answers 'Whatever.' to anything else.
+
+
+print("Task 1")
+text = input("Your question: ")
+flag1 = True
+flag2 = True
 for word in text:
     if word == '?':
         print('Bob answers: "Sure."')
+        flag1 = False
         break
     if word == '!':
         print('Bob answers: "Whoa, chill out!"')
+        flag2 = False
         break
-if text.isupper():
+if text.isupper() and flag2:
     print('Bob answers: "Whoa, chill out!"')
 elif text.isspace() | text.isdigit():
     print('Bob answers: "Fine. Be that way!"')
-else:
+elif flag1 and flag2:
     print('Whatever."')
 
+# Rna Transcription
+# Given a DNA strand, return its RNA complement (per RNA transcription).
+# Both DNA and RNA strands are a sequence of nucleotides.
 
-
-
-print("Select difficulty level: easy, middle, hard")
-dna = (input("Level: ")).upper()
+print("Task 2. GCTA -> CGAU")
+dna = (input("Value: ")).upper()
 # G -> C
 # C -> G
 # T -> A
-# A -> U
+# T -> U
 rna = ""
-for letter in dna:
-    if letter == 'G':
-        letter = 'C'
-    elif letter == 'C':
-        letter = 'G'
-    elif letter == 'T':
-        letter = 'A'
-    else:
-        letter = 'U'
-    rna += letter
+if dna.length >= 4:
+    for letter in dna:
+        if letter != 'G' and letter != 'C' and letter != 'T' and letter != 'T' :
+            print('Only : G-C-T-A')
+            break
+        elif letter == 'G':
+            letter = 'C'
+        elif letter == 'C':
+            letter = 'G'
+        elif letter == 'T':
+            letter = 'A'
+        else:
+            letter = 'U'
+        rna += letter
 
-print(rna)
+    print(rna)
+
+
+# Word Coun
+# Given a phrase, count the occurrences of each word in that phrase
+# For example for the input "olly olly in come free
+
 
 n = 0
-words = input("Level: ")
+words = input("Text: ")
 list_words = words.split()
 for w in list_words:
     if words.find(w) != -1:
         n += 1
 
-print(n)
+    print(w + ': ' + str(n))
+    n = 0
