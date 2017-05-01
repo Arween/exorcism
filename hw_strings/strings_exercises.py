22fd71365e07ed92a0c998dd08368eb32bfe0209
Arween <semka.alesya@gmail.com>
2017-05-01 11:50:04 +0300

    task3 2.0

--- hw_strings/strings_exercises.py
+++ hw_strings/strings_exercises.py
@@ -7,52 +7,61 @@
 
 
 print("Task 1")
-text = input("Your question: ")
-flag1 = True
-flag2 = True
-for word in text:
-    if word == '?':
-        print('Bob answers: "Sure."')
-        flag1 = False
-        break
-    if word == '!':
+
+def answer_Bob():
+
+    text = input("Your question: ")
+    flag1 = True
+    flag2 = True
+    for word in text:
+        if word == '?':
+            print('Bob answers: "Sure."')
+            flag1 = False
+            break
+        if word == '!':
+            print('Bob answers: "Whoa, chill out!"')
+            flag2 = False
+            break
+    if text.isupper() and flag2:
         print('Bob answers: "Whoa, chill out!"')
-        flag2 = False
-        break
-if text.isupper() and flag2:
-    print('Bob answers: "Whoa, chill out!"')
-elif text.isspace() | text.isdigit():
-    print('Bob answers: "Fine. Be that way!"')
-elif flag1 and flag2:
-    print('Whatever."')
+    elif text.isspace() | text.isdigit():
+        print('Bob answers: "Fine. Be that way!"')
+    elif flag1 and flag2:
+        print('Whatever."')
+
+answer_Bob()
 
 # Rna Transcription
 # Given a DNA strand, return its RNA complement (per RNA transcription).
 # Both DNA and RNA strands are a sequence of nucleotides.
 
 print("Task 2. GCTA -> CGAU")
-dna = (input("Value: ")).upper()
-# G -> C
-# C -> G
-# T -> A
-# T -> U
-rna = ""
-if dna.length >= 4:
-    for letter in dna:
-        if letter != 'G' and letter != 'C' and letter != 'T' and letter != 'T' :
-            print('Only : G-C-T-A')
-            break
-        elif letter == 'G':
-            letter = 'C'
-        elif letter == 'C':
-            letter = 'G'
-        elif letter == 'T':
-            letter = 'A'
-        else:
-            letter = 'U'
-        rna += letter
 
-    print(rna)
+def gcta_cgau():
+    dna = (input("Value: ")).upper()
+    # G -> C
+    # C -> G
+    # T -> A
+    # T -> U
+    rna = ""
+    if len(dna) >= 4:
+        for letter in dna:
+            if letter != 'G' and letter != 'C' and letter != 'T' and letter != 'T' :
+                print('Only : G-C-T-A')
+                break
+            elif letter == 'G':
+                letter = 'C'
+            elif letter == 'C':
+                letter = 'G'
+            elif letter == 'T':
+                letter = 'A'
+            else:
+                letter = 'U'
+            rna += letter
+
+        print(rna)
+    else: print('Less than 4')
+gcta_cgau()
 
 
 # Word Coun
@@ -60,14 +69,18 @@ if dna.length >= 4:
 # For example for the input "olly olly in come free
 
 
-n = 0
-words = input("Text: ")
-list_words = words.split()
-for w in list_words:
-    for w2 in list_words:
-        if w == w2:
-            n += 1
-    print(w + ': ' + str(n))
-    n = 0
+print("Word Count")
 
-    # n = 0
+def word_count():
+    words = input("Text: ")
+    my_words = words.lower().split()
+    my_dict = {}
+    for item in my_words:
+        if item in my_dict:
+            my_dict[item] += 1
+        else:
+            my_dict[item] = 1
+    print(my_dict)
+
+
+word_count()
